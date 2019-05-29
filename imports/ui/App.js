088a7d998f3e5997d8dc755d9df2d1d4b757ef57
blabuduua <31963350+blabuduua@users.noreply.github.com>
2019-05-29 10:30:37 +0300

    add new task inputer)

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { withTracker } from 'meteor/react-meteor-data';
- 
+
 import { Tasks } from '../api/tasks.js';
- 
+
 import Task from './Task.js';
- 
+
 // App component - represents the whole app
 class App extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // Find the text field via the React ref
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
   renderTasks() {
     return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
@@ -16,20 +32,28 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <header>
-          <h1>Todo List</h1>
-        </header>
- 
-        <ul>
+         <header>
+           <h1>Todo List</h1>
+
+           <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+             <input
+               type="text"
+               ref="textInput"
+               placeholder="Type to add new tasks"
+             />
+           </form>
+         </header>
+
+         <ul>
           {this.renderTasks()}
-        </ul>
+         </ul>
       </div>
     );
   }
 }
- 
+
 export default withTracker(() => {
   return {
-    tasks: Tasks.find({ 'text': 'Hello world!' }).fetch(),
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch()
   };
 })(App);
